0c9d97f92d4700eccf277877d3ade57c16b1f720
dimjad <230209501007@student.unm.ac.id>
2025-04-23 18:41:16 +0800

    manage icon into one icons component

---
+++ components/icons.tsx
@@ -0,0 +1,90 @@
+interface icon {
+    size?: number;
+    classname?: string;
+}
+export function MailIcon({ size = 24, classname = "fill-neutral-50" }: icon) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width={size}
+            height={size}
+            className={classname}
+        >
+            <path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM20 7.23792L12.0718 14.338L4 7.21594V19H20V7.23792ZM4.51146 5L12.0619 11.662L19.501 5H4.51146Z"></path>
+        </svg>
+    );
+}
+
+export function NotificationIcon({
+    size = 24,
+    classname = "fill-neutral-50",
+}: icon) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width={size}
+            height={size}
+            className={classname}
+        >
+            <path d="M20 17H22V19H2V17H4V10C4 5.58172 7.58172 2 12 2C16.4183 2 20 5.58172 20 10V17ZM9 21H15V23H9V21Z"></path>
+        </svg>
+    );
+}
+
+export function UserIcon({ size = 24, classname = "fill-neutral-50" }: icon) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width={size}
+            height={size}
+            className={classname}
+        >
+            <path d="M20 22H18V20C18 18.3431 16.6569 17 15 17H9C7.34315 17 6 18.3431 6 20V22H4V20C4 17.2386 6.23858 15 9 15H15C17.7614 15 20 17.2386 20 20V22ZM12 13C8.68629 13 6 10.3137 6 7C6 3.68629 8.68629 1 12 1C15.3137 1 18 3.68629 18 7C18 10.3137 15.3137 13 12 13ZM12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z"></path>
+        </svg>
+    );
+}
+
+export function KeyIcon({ size = 24, classname = "fill-neutral-50" }: icon) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width={size}
+            height={size}
+            className={classname}
+        >
+            <path d="M12.917 13C12.441 15.8377 9.973 18 7 18C3.68629 18 1 15.3137 1 12C1 8.68629 3.68629 6 7 6C9.973 6 12.441 8.16229 12.917 11H23V13H21V17H19V13H17V17H15V13H12.917ZM7 16C9.20914 16 11 14.2091 11 12C11 9.79086 9.20914 8 7 8C4.79086 8 3 9.79086 3 12C3 14.2091 4.79086 16 7 16Z"></path>
+        </svg>
+    );
+}
+
+export function EditIcon({ size }: { size: number }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width={size}
+            height={size}
+            className="fill-primary"
+        >
+            <path d="M6.41421 15.89L16.5563 5.74785L15.1421 4.33363L5 14.4758V15.89H6.41421ZM7.24264 17.89H3V13.6473L14.435 2.21231C14.8256 1.82179 15.4587 1.82179 15.8492 2.21231L18.6777 5.04074C19.0682 5.43126 19.0682 6.06443 18.6777 6.45495L7.24264 17.89ZM3 19.89H21V21.89H3V19.89Z"></path>
+        </svg>
+    );
+}
+
+export function DeleteIcon({ size }: { size: number }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="fill-primary"
+            width={size}
+            height={size}
+        >
+            <path d="M7 4V2H17V4H22V6H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V6H2V4H7ZM6 6V20H18V6H6ZM9 9H11V17H9V9ZM13 9H15V17H13V9Z"></path>
+        </svg>
+    );
+}
--- components/task-item.tsx
+++ components/task-item.tsx
@@ -1,6 +1,19 @@
 "use client";
 import { Input } from "./ui/input";
 import { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
 interface TaskItemProps {
     task: string;
     isDone: boolean;
@@ -11,35 +24,7 @@ interface TaskItemProps {
     // onDelete: (id: number) => void;
     // onDone: (id: number) => void;
 }
-
-function EditIcon({ size }: { size: number }) {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width={size}
-            height={size}
-            className="fill-primary"
-        >
-            <path d="M6.41421 15.89L16.5563 5.74785L15.1421 4.33363L5 14.4758V15.89H6.41421ZM7.24264 17.89H3V13.6473L14.435 2.21231C14.8256 1.82179 15.4587 1.82179 15.8492 2.21231L18.6777 5.04074C19.0682 5.43126 19.0682 6.06443 18.6777 6.45495L7.24264 17.89ZM3 19.89H21V21.89H3V19.89Z"></path>
-        </svg>
-    );
-}
-
-function DeleteIcon({ size }: { size: number }) {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            className="fill-primary"
-            width={size}
-            height={size}
-        >
-            <path d="M7 4V2H17V4H22V6H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V6H2V4H7ZM6 6V20H18V6H6ZM9 9H11V17H9V9ZM13 9H15V17H13V9Z"></path>
-        </svg>
-    );
-}
-
+import { EditIcon, DeleteIcon } from "@/components/icons";
 export default function TaskItem({ id, task, isDone }: TaskItemProps) {
     const [isTaskDone, setIsTaskDone] = useState(isDone);
 
@@ -54,14 +39,12 @@ export default function TaskItem({ id, task, isDone }: TaskItemProps) {
                     <Input
                         type="checkbox"
                         className="accent-secondary w-4 h-4"
-                        checked={isTaskDone}
-                        readOnly
-                        onChange={(e) => {
-                            if (e.target.checked) {
-                                alert("done");
-                            }
-                            setIsTaskDone(e.target.checked);
-                        }}
+                        // checked={isTaskDone}
+                        // onChange={(e) => {
+                        //     if (e.target.checked) {
+                        //     }
+                        //     setIsTaskDone(e.target.checked);
+                        // }}
                     />
                 </div>
 
@@ -76,13 +59,53 @@ export default function TaskItem({ id, task, isDone }: TaskItemProps) {
                 {/**
                  * @TODO : handle edit by displaying popup form
                  */}
-                <button
-                    onClick={() => {
-                        alert(`Edit ${id}`);
-                    }}
-                >
-                    <EditIcon size={20} />
-                </button>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <button>
+                            <EditIcon size={20} />
+                        </button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle>Edit Tugas</DialogTitle>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                            <div className="grid w-full gap-1.5">
+                                <Label htmlFor="title">Judul Tugas</Label>
+                                <Input
+                                    id="title"
+                                    name="title"
+                                    defaultValue={task}
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <div className="grid w-full gap-1.5">
+                                <Label htmlFor="description">Deskripsi</Label>
+                                <Textarea
+                                    className="col-span-3 border-secondary rounded-none"
+                                    name="description"
+                                    placeholder="Type your message here."
+                                    id="description"
+                                />
+                            </div>
+                            <div className="grid w-full gap-1.5">
+                                <Input type="date" />
+                                <Input type="time" />
+                            </div>
+                        </div>
+                        <DialogFooter>
+                            <DialogClose asChild>
+                                <Button
+                                    variant="outline"
+                                    className="border-secondary hover:bg-primary/70 hover:text-primary-foreground"
+                                >
+                                    Batal
+                                </Button>
+                            </DialogClose>
+                            <Button type="submit">Simpan</Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
 
                 {/**
                  * @TODO : handle delete by displaying popup alert
